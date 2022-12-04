1f561336083e5174855251c9b14cedf63b72212f
Mark Abrams <mabrams@helpguest.com>
2022-12-04 11:24:03 -0500

    just some basic presentation with my components

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import React from 'react'
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+	  <Image
+	  	source={require('./assets/clock/numbers/one.png')}
+		style={{width:100, height:100, tintColor: 'rgba(12,200,110,1)', backgroundColor: 'rgba(128,19,210,1)', borderBottomLeftRadius: 20, borderTopLeftRadius: 20,resizeMode:'contain'}}
+		/>
+	  <Image
+	  	source={require('./assets/clock/numbers/two.png')}
+		style={{width:100, height:100, tintColor: 'rgba(12,200,110,1)', backgroundColor: 'rgba(128,19,210,1)', borderBottomLeftRadius: 20, borderTopLeftRadius: 20,resizeMode:'contain'}}
+		/>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +22,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'rgba(128,19,210,0)',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+export default App;
