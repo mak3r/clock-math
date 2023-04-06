d4d627938a49f003470cc4f603d0e502ca310c52
Mark Abrams <mabrams@helpguest.com>
2023-04-06 14:30:02 -0400

    updates

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import React from 'react'
@@ -27,5 +28,71 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+=======
+import React, { useRef, useState } from 'react';
+import { View, Image, PanResponder, Animated } from 'react-native';
+
+const App = () => {
+  const pan1 = useRef(new Animated.ValueXY()).current;
+  const pan2 = useRef(new Animated.ValueXY()).current;
+  const [isColliding, setIsColliding] = useState(false);
+
+  const panResponder1 = useRef(
+    PanResponder.create({
+      onMoveShouldSetPanResponder: () => true,
+      onPanResponderMove: Animated.event([null, { dx: pan1.x, dy: pan1.y }]),
+      onPanResponderRelease: (e, gestureState) => {
+        if (isColliding) {
+          Animated.spring(pan2, { toValue: { x: -200, y: 0 }, useNativeDriver: true }).start();
+        }
+      },
+    })
+  ).current;
+
+  const panResponder2 = useRef(
+    PanResponder.create({
+      onMoveShouldSetPanResponder: () => true,
+      onPanResponderMove: Animated.event([null, { dx: pan2.x, dy: pan2.y }]),
+    })
+  ).current;
+
+  const checkCollision = () => {
+    const image1Position = {
+      x: pan1.x._value + 50, // assuming image1 width is 100
+      y: pan1.y._value + 50, // assuming image1 height is 100
+    };
+
+    const image2Position = {
+      x: pan2.x._value + 50, // assuming image2 width is 100
+      y: pan2.y._value + 50, // assuming image2 height is 100
+    };
+
+    const collision =
+      image1Position.x >= image2Position.x &&
+      image1Position.x <= image2Position.x + 100 &&
+      image1Position.y >= image2Position.y &&
+      image1Position.y <= image2Position.y + 100;
+
+    setIsColliding(collision);
+  };
+
+  return (
+    <View>
+      <Animated.Image
+        source={require('./assets/clock/numbers/one.png')}
+        {...panResponder1.panHandlers}
+        style={{ transform: [{ translateX: pan1.x }, { translateY: pan1.y }] }}
+        onLayout={checkCollision}
+      />
+      <Animated.Image
+        source={require('./assets/clock/numbers/two.png')}
+        {...panResponder2.panHandlers}
+        style={{ transform: [{ translateX: pan2.x }, { translateY: pan2.y }] }}
+        onLayout={checkCollision}
+      />
+    </View>
+  );
+};
+>>>>>>> Stashed changes
 
 export default App;
